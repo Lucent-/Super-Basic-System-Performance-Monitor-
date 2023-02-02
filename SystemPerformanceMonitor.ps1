33474708aab3b3ea3f728a96dd84ca9bc7837f55
Lu <Lucent-@users.noreply.github.com>
2023-02-02 12:48:49 +0100

    Update SystemPerformanceMonitor.ps1

--- SystemPerformanceMonitor.ps1
+++ SystemPerformanceMonitor.ps1
@@ -1,11 +1,14 @@
-$interval = 5
+$interval = 2
 
 while ($true)
 {
+
     $cpuUsage = Get-Counter -Counter "\Processor(_Total)\% Processor Time" | Select-Object -ExpandProperty CounterSamples | Select-Object -ExpandProperty CookedValue
     $memoryUsage = Get-Counter -Counter "\Memory\Available MBytes" | Select-Object -ExpandProperty CounterSamples | Select-Object -ExpandProperty CookedValue
     $diskUsage = Get-Counter -Counter "\PhysicalDisk(_Total)\% Disk Time" | Select-Object -ExpandProperty CounterSamples | Select-Object -ExpandProperty CookedValue
     $networkUsage = Get-Counter -Counter "\Network Interface(*)\Bytes Total/sec" | Select-Object -ExpandProperty CounterSamples | Select-Object -ExpandProperty CookedValue
+
+    Clear-Host;    
 
     Write-Output "CPU Usage: $cpuUsage %"
     Write-Output "Memory Usage: $memoryUsage MB"
@@ -13,4 +16,5 @@ while ($true)
     Write-Output "Network Usage: $networkUsage Bytes/sec"
 
     Start-Sleep -Seconds $interval
+	
 }
